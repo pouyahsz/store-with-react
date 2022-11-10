76dd314ae84b462d8d845a887dde8c23bd2a7431
pouya <hassanalizadeh@gmail.com>
2022-11-10 11:49:07 +0330

    responsive footer was created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Products from "./components/pages/Products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      setProducts(data);
+    }
+    fetchData()
+  }, [])
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Products />} />
+            <Route index element={<Products products={products} />} />
           </Route>
         </Routes>
       </BrowserRouter >
